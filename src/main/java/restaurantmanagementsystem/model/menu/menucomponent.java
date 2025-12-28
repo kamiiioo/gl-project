59727b3f5f7db555c95d796501bfde7a6c30ea94
Nazim <nazimguerguadj@gmail.com>
2025-12-28 11:46:27 +0100

    Update menucomponent.java

--- src/main/java/restaurantmanagementsystem/model/menu/menucomponent.java
+++ src/main/java/restaurantmanagementsystem/model/menu/menucomponent.java
@@ -1,4 +1,24 @@
 package restaurantmanagementsystem.model.menu;
 
-public class menucomponent {
+public abstract class MenuComponent {
+
+    public void add(MenuComponent component) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent component) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void display() {
+        throw new UnsupportedOperationException();
+    }
 }
