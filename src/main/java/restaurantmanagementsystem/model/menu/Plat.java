ecd12324744a2bc43967e444302a5d9867015155
Nazim <nazimguerguadj@gmail.com>
2025-12-28 11:47:09 +0100

    Update Plat.java

--- src/main/java/restaurantmanagementsystem/model/menu/Plat.java
+++ src/main/java/restaurantmanagementsystem/model/menu/Plat.java
@@ -1,4 +1,27 @@
 package restaurantmanagementsystem.model.menu;
 
-public class Plat {
+public class Plat extends MenuComponent {
+
+    private String name;
+    private double price;
+
+    public Plat(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("- " + name + " : " + price + " DA");
+    }
 }
