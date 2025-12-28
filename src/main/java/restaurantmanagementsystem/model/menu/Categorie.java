7e2984038e3a29f821b885401baf116536757088
Nazim <nazimguerguadj@gmail.com>
2025-12-28 11:47:31 +0100

    Update Categorie.java

--- src/main/java/restaurantmanagementsystem/model/menu/Categorie.java
+++ src/main/java/restaurantmanagementsystem/model/menu/Categorie.java
@@ -1,4 +1,38 @@
 package restaurantmanagementsystem.model.menu;
 
-public class Categorie {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categorie extends MenuComponent {
+
+    private String name;
+    private List<MenuComponent> components = new ArrayList<>();
+
+    public Categorie(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void add(MenuComponent component) {
+        components.add(component);
+    }
+
+    @Override
+    public void remove(MenuComponent component) {
+        components.remove(component);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("\n== " + name + " ==");
+        for (MenuComponent component : components) {
+            component.display();
+        }
+      
+    }
 }
